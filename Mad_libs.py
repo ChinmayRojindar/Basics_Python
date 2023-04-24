26d5c4394c5ffc12f33b395e9246f2bb1f076c1a
Chinmay_Rojindar <84935908+ChinmayRojindar@users.noreply.github.com>
2023-04-24 23:28:51 +0530

    Add files via upload
    Worst way to do string concatenation

---
+++ Mad_libs.py
@@ -0,0 +1,12 @@
+verb1=input("verb1: ")
+verb2=input("verb2: ")
+verb3=input("verb3: ")
+adj=input("adj: ")
+Noun=input("Noun: ")
+famous_person=input("Famous Person: ")
+
+
+Madlibs="I" + verb1+"in the morning, then I brush my"+ Noun + verb2 +"in the office"+"have a meeting with the"+ adj + famous_person\
+    +"and" + verb3 + "with them, quite a day."
+
+print(Madlibs)
